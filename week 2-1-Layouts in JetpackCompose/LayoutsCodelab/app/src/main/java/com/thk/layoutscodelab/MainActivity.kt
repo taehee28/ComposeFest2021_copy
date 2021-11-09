d3109668a3a0a3a007cfb681b3c92ac4399850c3
taehee28 <dev.thk28@gmail.com>
2021-11-09 17:56:04 +0900

    Step 3. Modifiers 진행 완료

--- week 2-1-Layouts in JetpackCompose/LayoutsCodelab/app/src/main/java/com/thk/layoutscodelab/MainActivity.kt
+++ week 2-1-Layouts in JetpackCompose/LayoutsCodelab/app/src/main/java/com/thk/layoutscodelab/MainActivity.kt
@@ -3,11 +3,19 @@ package com.thk.layoutscodelab
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.thk.layoutscodelab.ui.theme.LayoutsCodelabTheme
 
 class MainActivity : ComponentActivity() {
@@ -21,6 +29,44 @@ class MainActivity : ComponentActivity() {
                 }
             }
         }
+    }
+}
+
+@Composable
+fun PhotographerCard() {
+    Row {
+        // 이미지의 플레이스홀더
+        Surface(
+            modifier = Modifier.size(50.dp),
+            shape = CircleShape,
+            color = MaterialTheme.colors.onSurface.copy(alpha = 0.2f)
+        ) {
+
+        }
+
+        // Modifier 체이닝은 작성한 순서대로 컴포저블에 적용되기 때문에 순서를 고려해서 작성해야 함
+        Column(
+            modifier = Modifier
+                .padding(start = 8.dp)
+                .align(Alignment.CenterVertically)  // 레이아웃 관련 modifier. 레이아웃 별로 받을 수 있는 관련 modifier가 정해져 있다. 컴파일타임에 체크함
+        ) {
+            Text(text = "Alfred Sisley", fontWeight = FontWeight.Bold)
+
+            // 컴포지션 트리에 명시적으로 값을 넘기게 해주는 프로바이더
+            // 자식으로 있는 컴포저블의 불투명도 설정하기 위해 LocalContentAlpha 사용
+            CompositionLocalProvider(LocalContentAlpha provides ContentAlpha.medium) {
+                Text(text = "3 minutes ago", style = MaterialTheme.typography.body2)
+            }
+        }
+    }
+
+}
+
+@Preview
+@Composable
+fun PhotographerCardPreview() {
+    LayoutsCodelabTheme {
+        PhotographerCard()
     }
 }
 
