a52db64fa78fd0be76482af0707946f9fe188df8
taehee28 <dev.thk28@gmail.com>
2021-11-12 16:51:36 +0900

    Step 7. Create your custom layout 진행 완료

--- week 2-1-Layouts in JetpackCompose/LayoutsCodelab/app/src/main/java/com/thk/layoutscodelab/MainActivity.kt
+++ week 2-1-Layouts in JetpackCompose/LayoutsCodelab/app/src/main/java/com/thk/layoutscodelab/MainActivity.kt
@@ -17,8 +17,14 @@ import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.AlignmentLine
+import androidx.compose.ui.layout.FirstBaseline
+import androidx.compose.ui.layout.Layout
+import androidx.compose.ui.layout.MeasurePolicy
+import androidx.compose.ui.layout.layout
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import coil.compose.rememberImagePainter
 import com.thk.layoutscodelab.ui.theme.LayoutsCodelabTheme
@@ -32,6 +38,74 @@ class MainActivity : ComponentActivity() {
                 ImageList()
             }
         }
+    }
+}
+
+// Custom Composable
+@Composable
+fun MyOwnColumn(modifier: Modifier = Modifier, content: @Composable () -> Unit) {
+    Layout(content = content, modifier = modifier) { measurables, constraints ->
+        // 측정된 children 에 대한 리스트
+        val placeables = measurables.map { measurable ->
+            measurable.measure(constraints)
+        }
+
+        // column 처럼 아이템을 쌓기 위해서 y 좌표가 쌓이는 아이템만큼 늘어나야 함
+        var yPosition = 0
+
+        // 레이아웃의 사이즈 지정. 부모만큼 크게할거라서 넘어온 constraints의 max값 사용
+        layout(constraints.maxWidth, constraints.maxHeight) {
+            // place children
+
+            // 리스트니까 각각의 placeable 만큼 반복
+            placeables.forEach { placeable ->
+                placeable.placeRelative(x = 0, y = yPosition)
+
+                // 그 다음 아이템이 놓일 y 좌표 계산
+                yPosition += placeable.height
+            }
+        }
+    }
+}
+
+
+// Custom Layout Modifier
+fun Modifier.firstBaselineToTop(firstBaselineToTop: Dp) = this.then(    // TODO: 왜 this.then()을 쓰는지 찾아보기
+    layout { measurable, constraints ->     // measurable = 측정 및 배치할 child, constraints = child의 넓이,높이의 최대/최소값
+
+        // 딱 한번 child를 측정
+        val placeable = measurable.measure(constraints = constraints)   // constraints 는 직접 만들어도 되고, 그냥 넘어오는거 넘겨도 되고
+
+        // 컴포저블이 베이스라인을 가지는지 확인
+        check(placeable[FirstBaseline] != AlignmentLine.Unspecified)    // 특정 alignment의 포지션을 가져와서 값이 없는지(Unspecified) 확인
+        val firstBaseline = placeable[FirstBaseline]
+        // TODO: Alignment의 position이라는게 어떻게 표현되는 Int 값인지??
+
+        val placeableY = firstBaselineToTop.roundToPx() - firstBaseline     // 설정하고 싶은 padding 에서 baseline 위치만큼 뺌
+        val height = placeable.height + placeableY      // placeable의 높이(child의 높이)에 계산한 padding 더하기
+
+        // measure 단계가 끝났으니까, layout을 호출해서 사이즈를 계산하고 명시해줘야 함
+        // 람다로 넘기는 placementBlock 안에 child를 위치시키기 위한 코드를 적어줘야 함
+        layout(placeable.width, height) {
+            // 이걸 호출해야 화면에 나타남. 호출하면 알아서 현재의 layoutDirection에 맞춰서 잘 포지셔닝 해줌
+            placeable.placeRelative(0, placeableY)
+        }
+    }
+)
+
+@Preview
+@Composable
+fun TextWithPaddingToBaselinePreview() {    // 베이스라인부터 시작하는 패딩 적용
+    LayoutsCodelabTheme {
+        Text("Hi there!", Modifier.firstBaselineToTop(32.dp))
+    }
+}
+
+@Preview
+@Composable
+fun TextWithNormalPaddingPreview() {    // 그냥 패딩 적용
+    LayoutsCodelabTheme {
+        Text("Hi there!", Modifier.padding(top = 32.dp))
     }
 }
 
@@ -120,16 +194,26 @@ fun LayoutsCodelab() {
             }
         }
     ) { innerPadding ->
-        BodyContent(Modifier.padding(innerPadding).padding(8.dp))
+        BodyContent(
+            Modifier
+                .padding(innerPadding)
+                .padding(8.dp))
     }
 }
 
 @Composable
 fun BodyContent(modifier: Modifier = Modifier) {
-    Column(modifier = modifier) {
-        Text(text = "Hi there!")
-        Text(text = "Thanks for going through the Layouts codelab")
+    MyOwnColumn(modifier.padding(8.dp)) {
+        Text(text = "MyOwnColumn")
+        Text(text = "아이템을")
+        Text(text = "수직으로 표시한다")
+        Text(text = "내가 직접 만듬~~~")
     }
+
+//    Column(modifier = modifier) {
+//        Text(text = "Hi there!")
+//        Text(text = "Thanks for going through the Layouts codelab")
+//    }
 }
 
 @Preview
