d0db66c05e14d3540331a625f0ca7e18e965486e
taehee28 <dev.thk28@gmail.com>
2022-01-19 16:59:25 +0900

    Step 6. Extract finished NavHost 진행 완료

--- week 3-3-Jetpack Compose Navigation/app/src/main/java/com/example/compose/rally/RallyActivity.kt
+++ week 3-3-Jetpack Compose Navigation/app/src/main/java/com/example/compose/rally/RallyActivity.kt
@@ -29,14 +29,11 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import androidx.navigation.NavController
-import androidx.navigation.NavType
+import androidx.navigation.*
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
-import androidx.navigation.navArgument
-import androidx.navigation.navDeepLink
 import com.example.compose.rally.data.UserData
 import com.example.compose.rally.ui.accounts.AccountsBody
 import com.example.compose.rally.ui.accounts.SingleAccountBody
@@ -78,48 +75,7 @@ fun RallyApp() {
                 )
             }
         ) { innerPadding ->
-            NavHost(
-                navController = navController,
-                startDestination = RallyScreen.Overview.name,   // 시작점
-                modifier = Modifier.padding(innerPadding)
-            ) {
-                // composable 확장함수로 감싸진 하나 하나가 네비게이션 그래프에 목적지로 추가됨  
-                composable(route = RallyScreen.Overview.name) {
-                    OverviewBody(
-                        onClickSeeAllAccounts = { navController.navigate(RallyScreen.Accounts.name) },
-                        onAccountClick = { name -> navigateToSingleAccount(navController, name) },
-                        onClickSeeAllBills = { navController.navigate(RallyScreen.Bills.name) }
-                    )
-                }
-                composable(route = RallyScreen.Accounts.name) {
-                    AccountsBody(accounts = UserData.accounts) { name ->    // onAccountClick
-                        navigateToSingleAccount(navController, name)
-                    }
-                }
-                val accountsName = RallyScreen.Accounts.name
-                composable(
-                    route = "$accountsName/{name}",
-                    arguments = listOf(
-                        navArgument("name") {
-                            type = NavType.StringType
-                        }
-                    ),
-                    // 외부에서 해당 앱의 특정 화면을 열 수 있도록 딥링크 추가
-                    deepLinks = listOf(navDeepLink {
-                        uriPattern = "rally://$accountsName/{name}"
-                    })
-                ) { entry ->
-                    // composable의 arguments 항목에 넘긴 인자들을 여기서 파라미터인 entry로 꺼내쓸 수 있음
-                    // 전달된 argument로 destination에 필요한 요소를 찾음
-                    val accountName = entry.arguments?.getString("name")
-                    val account = UserData.getAccount(accountName)
-                    SingleAccountBody(account = account)
-                }
-
-                composable(route = RallyScreen.Bills.name) {
-                    BillsBody(bills = UserData.bills)
-                }
-            }
+           RallyNavHost(navController = navController, modifier = Modifier.padding(innerPadding))
 
 //            Box(Modifier.padding(innerPadding)) {
 //                currentScreen.content(
@@ -128,6 +84,55 @@ fun RallyApp() {
 //                    }
 //                )
 //            }
+        }
+    }
+}
+
+@Composable
+fun RallyNavHost(   // extracted
+    navController: NavHostController,
+    modifier: Modifier
+) {
+    NavHost(
+        navController = navController,
+        startDestination = RallyScreen.Overview.name,   // 시작점
+        modifier = modifier
+    ) {
+        // composable 확장함수로 감싸진 하나 하나가 네비게이션 그래프에 목적지로 추가됨
+        composable(route = RallyScreen.Overview.name) {
+            OverviewBody(
+                onClickSeeAllAccounts = { navController.navigate(RallyScreen.Accounts.name) },
+                onAccountClick = { name -> navigateToSingleAccount(navController, name) },
+                onClickSeeAllBills = { navController.navigate(RallyScreen.Bills.name) }
+            )
+        }
+        composable(route = RallyScreen.Accounts.name) {
+            AccountsBody(accounts = UserData.accounts) { name ->    // onAccountClick
+                navigateToSingleAccount(navController, name)
+            }
+        }
+        val accountsName = RallyScreen.Accounts.name
+        composable(
+            route = "$accountsName/{name}",
+            arguments = listOf(
+                navArgument("name") {
+                    type = NavType.StringType
+                }
+            ),
+            // 외부에서 해당 앱의 특정 화면을 열 수 있도록 딥링크 추가
+            deepLinks = listOf(navDeepLink {
+                uriPattern = "rally://$accountsName/{name}"
+            })
+        ) { entry ->
+            // composable의 arguments 항목에 넘긴 인자들을 여기서 파라미터인 entry로 꺼내쓸 수 있음
+            // 전달된 argument로 destination에 필요한 요소를 찾음
+            val accountName = entry.arguments?.getString("name")
+            val account = UserData.getAccount(accountName)
+            SingleAccountBody(account = account)
+        }
+
+        composable(route = RallyScreen.Bills.name) {
+            BillsBody(bills = UserData.bills)
         }
     }
 }
