826e7c4ed17ead43abd01a3770f01722f90c688c
taehee28 <dev.thk28@gmail.com>
2022-01-23 21:53:57 +0900

    Step 4.Debugging tests까지 진행 완료

---
+++ week 4-1-Testing in Jetpack Compose/app/src/androidTest/java/com/example/compose/rally/TopAppBarTest.kt
@@ -0,0 +1,53 @@
+package com.example.compose.rally
+
+import androidx.compose.material.Text
+import androidx.compose.ui.test.*
+import androidx.compose.ui.test.junit4.createComposeRule
+import androidx.compose.ui.text.toUpperCase
+import com.example.compose.rally.ui.components.RallyTopAppBar
+import org.junit.Rule
+import org.junit.Test
+
+class TopAppBarTest {
+
+    @get:Rule
+    val composeTestRule = createComposeRule()   // Compose 테스트를 위한 Rule
+
+    @Test
+    fun rallyTopAppBarTest() {
+        val allScreens = RallyScreen.values().toList()
+
+        composeTestRule.setContent {
+            RallyTopAppBar(
+                allScreens = allScreens,
+                onTabSelected = { },
+                currentScreen = RallyScreen.Accounts
+            )
+        }
+
+        composeTestRule
+            .onNodeWithContentDescription(RallyScreen.Accounts.name)    // finder
+            .assertIsSelected()     //assertion
+    }
+
+    @Test
+    fun rallyTopAppBarTest_currentLabelExists() {
+        val allScreens = RallyScreen.values().toList()
+
+        composeTestRule.setContent {
+            RallyTopAppBar(
+                allScreens = allScreens,
+                onTabSelected = {},
+                currentScreen = RallyScreen.Accounts
+            )
+        }
+
+        composeTestRule.onRoot().printToLog("currentLabelExists")
+
+        composeTestRule
+//            .onNodeWithText(RallyScreen.Accounts.name.uppercase())
+            .onNodeWithContentDescription(RallyScreen.Accounts.name)
+            .assertExists()
+    }
+
+}
